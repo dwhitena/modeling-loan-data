468acfc04549f3375a8d71e3ad8b4c3848980e36
dwhitena <whitenack.daniel@gmail.com>
2015-03-27 17:12:51 -0500

    update model to include dependence on log(annual income)

--- multivariate/multivariate.py
+++ multivariate/multivariate.py
@@ -6,7 +6,7 @@ import statsmodels.formula.api as smf
 
 
 # load the 2014 lending club statistics
-df = pd.read_csv('LoanStats3c.csv', skiprows=1)
+df = pd.read_csv('LoanStats3c.csv', skiprows=1, low_memory=False)
 
 
 # ---------------
@@ -15,8 +15,7 @@ df = pd.read_csv('LoanStats3c.csv', skiprows=1)
 
 # remove null values and "NaN"
 df = df[['int_rate','annual_inc','home_ownership']]
-df.dropna(inplace=True)
-df = df[df.int_rate != 'NaN']
+df.dropna(subset=['issue_d'], inplace=True)
 
 # strip percent signs in int_rate and covert to float
 df['int_rate'] = df['int_rate'].map(lambda \
@@ -25,6 +24,9 @@ df['int_rate'] = df['int_rate'].map(lambda \
 # convert incomes to integers
 df['annual_inc'] = df['annual_inc'].map(lambda element: int(element))
 
+# transform income via log, base 10 (because correlation with annual_inc is weak)
+df['log10_inc'] = df['annual_inc'].map(lambda num: np.log10(num))
+
 
 # -------------------------------
 # MODEL INTEREST RATE VS. INCOME
@@ -32,13 +34,14 @@ df['annual_inc'] = df['annual_inc'].map(lambda element: int(element))
 
 # variables
 intrate = df['int_rate']
-annualincome = df['annual_inc']
+#annualincome = df['annual_inc']
+annualincomelog = df['log10_inc']
 
 # shape data
 # The dependent variable
 y = np.matrix(intrate).transpose()
 # The independent variables shaped as columns
-x = np.matrix(annualincome).transpose()
+x = np.matrix(annualincomelog).transpose()
 
 # add column of ones (constant)
 X = sm.add_constant(x)
@@ -50,16 +53,14 @@ print 'Intercept: ', model1.params[0]
 print 'P-Values: ', model1.pvalues  # ??? why do these come out zero ???
 print 'R-Squared: ', model1.rsquared
 
-# NOTE: this does not appear to model the data very well.  The intercept, is
-# basically the mean of the interest rates, and the r-squared indicates that
-# the model does not fit the data very well. The coefficient of the annual 
-# income is so small, it seems negligible.
 # To visualize this see the following plot:
+logsample = np.arange(min(df['log10_inc']), max(df['log10_inc']), 0.1)
 plt.figure()
-plt.scatter(df['annual_inc'], \
+plt.scatter(df['log10_inc'], \
 	df['int_rate'], alpha=0.3)
-plt.xlabel('Annual Income')
+plt.xlabel('log(Annual Income)')
 plt.ylabel('Interest Rate')
+plt.plot(model1.params[0] + model1.params[1]*logsample, 'r')
 plt.draw()
 plt.savefig('rate_vs_income.png')
 
@@ -73,7 +74,7 @@ plt.savefig('rate_vs_income.png')
 df['home_dummy'] = pd.Categorical(df.home_ownership).labels
 
 # Multiple regression model
-model2 = smf.ols(formula="int_rate ~ home_dummy + annual_inc", data=df).fit()
+model2 = smf.ols(formula="int_rate ~ home_dummy + log10_inc", data=df).fit()
 print model2.summary()
 
 
@@ -86,5 +87,5 @@ print model2.summary()
 df['home_dummy'] = pd.Categorical(df.home_ownership).labels
 
 # Multiple regression model
-model3 = smf.ols(formula="int_rate ~ home_dummy*annual_inc", data=df).fit()
+model3 = smf.ols(formula="int_rate ~ home_dummy*log10_inc", data=df).fit()
 print model3.summary()
